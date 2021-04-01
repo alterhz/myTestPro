f4d59d077b87daae2ef060c917cc3e49eee0d63b
550360139@qq.com <550360139@qq.com>
2021-04-02 03:26:49 +0800

    - 1.2.2-SNAPSHOT 1. js导致加载慢的bug 2. 保持登陆的bug
    - 1.2.1-SNAPSHOT
    1. js导致加载慢的bug

--- magazine/src/main/java/com/magazine/config/SecurityConfig.java
+++ magazine/src/main/java/com/magazine/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.magazine.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +15,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,6 +36,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .rememberMe()
                     .tokenValiditySeconds(2419200)
                     .key("BookSecured");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
     }
 
     @Bean
