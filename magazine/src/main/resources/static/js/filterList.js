815f0ad08e565ee37cc8b85b148969004c807cdc
550360139@qq.com <550360139@qq.com>
2021-04-19 20:06:39 +0800

    编辑

--- magazine/src/main/resources/static/js/filterList.js
+++ magazine/src/main/resources/static/js/filterList.js
@@ -14,7 +14,15 @@ axios.get('/filters')
         console.log(response);
         responseData = response.data;
 
+        // 处理filters.给每个filter添加addField字段
+        for (var key in responseData) {
+            console.log(responseData[key].sheetName);
+            responseData[key].addField = "empty";
+        }
+
         myFilter.filters = responseData;
+
+
     })
     .catch(function(error) {
         console.log(error);
