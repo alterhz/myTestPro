9a95b6c7662e683e625006ce2722d212168f7e4a
550360139@qq.com <550360139@qq.com>
2021-04-23 21:28:48 +0800

    js跨域问题的解决方案

--- magazine/src/main/java/com/magazine/config/WebMvcConfig.java
+++ magazine/src/main/java/com/magazine/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.magazine.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    /**
+     * js跨域问题的解决方案
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(false) //注意这里为false
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 }
